76b9602496463c44ea840030673b3eb294e054bc
milekb <milosz_bienkowski@vp.pl>
2016-05-05 18:39:24 +0200

    Create zad1.c

---
+++ LAB8/zad1.c
@@ -0,0 +1,8 @@
+
+
+#define INFO(kraj, waluta) W kraj OBOWIAZUJE waluta.
+INFO(Polsce, zloty)
+INFO(Rosji, rubel)
+INFO(Slowacji, euro)
+
+//Zostana wypisane: W Polsce OBOWIAZUJE zloty. W Rosji OBOWIAZUJE rubel. W Slowacji OBOWIAZUJE euro.
